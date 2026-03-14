871199f402288480a3d29eb1022bb27ec699bb76
yuvi-mobile-apps <yuvir1958@gmail.com>
2026-03-14 08:59:48 +0530

    changes made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 "use client"
 
+
+import TextBox from "@/components/TextBox";
+
+
+
+
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
-      <h1 className="border border-white h-16 w-32 flex items-center justify-center p-4 rounded-xl cursor-pointer" onClick={() => alert("hello world")}>Hello World</h1>
+    <div className="min-h-screen min-w-screen container" id="bounds">
+      <TextBox />
     </div>
   );
 }
